fdbe654d51fed0b86826a3081d48a900ec1ef022
satoru <funai@ossl.co.jp>
2016-04-08 23:18:59 +0900

    First commit

---
+++ roles/tomcat/files/setenv.sh
@@ -0,0 +1,9 @@
+JAVA_HOME=/usr/java/default
+JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms256m -Xmx1024m -XX:MaxPermSize=256M -server"
+LANG="ja_JP.UTF-8"
+
+#以下JMX監視用
+#CATALINA_OPTS="-Dcom.sun.management.jmxremote=true
+#               -Dcom.sun.management.jmxremote.port=10080
+#               -Dcom.sun.management.jmxremote.ssl=false
+#               -Dcom.sun.management.jmxremote.authenticate=false"
